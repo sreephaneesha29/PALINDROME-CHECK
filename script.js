d7ed93ec88964c8f1f11ff1bcfa869d1e95cc374
sreephaneesha <sreephaneesha2005@gmail.com>
2024-03-03 19:28:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ const resetInput = () => {
 
 const checkPalindrome = () => {
     const inputValue = ogInput.value.trim();
+    const processedInput = inputValue.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    const reversedInput = processedInput.split("").reverse().join("");
     if (inputValue === "") {
         alert("Please input a value");
         return;
     }
     
-    const processedInput = inputValue.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
-    const reversedInput = processedInput.split("").reverse().join("");
     
     if (processedInput === reversedInput) {
         result.innerHTML = `${inputValue.bold()} is a palindrome`;
